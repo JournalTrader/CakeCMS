29bef16aa8f5cacd74dc52ae00a0a7a89fe97e97
JournalTrader <moricet.nicolas@free.fr>
2012-10-12 11:53:57 +0200

    Le projet permet l'intégration d'élément avec la possibilité de changer des donnée dans une table et de supprimer du contenu.
    Module Seo (manager) = ok
    Module Page avec intégration du module Seo = ok
    
    Modification de tools.js pour la confirmation d'une suppression (confirmBox). Si c'est dans un formulaire cç envoie un submit avec le nom des blocks pour avoir les liaisons avec les éléments liés.
    
    Cela permet ainsi d'envoyer des information sur des éléments liés
    
    En cour de création du module Blog

---
+++ app/Plugin/Blog/Controller/BlogAppController.php
@@ -0,0 +1,6 @@
+<?php
+
+class BlogAppController extends AppController {
+
+}
+
---
+++ app/Plugin/Blog/Controller/BlogController.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of BlogController
+ *
+ * @author nicolasmoricet
+ */
+class BlogController extends BlogAppController
+{
+    public function manager_index()
+    {
+        
+    }
+    
+    public function manager_add()
+    {
+        
+    }
+}
+
+?>
