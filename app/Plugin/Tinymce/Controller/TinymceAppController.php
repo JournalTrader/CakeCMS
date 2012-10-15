8808e056f72592b72651a899a1046a8d6093cc94
JournalTrader <moricet.nicolas@free.fr>
2012-10-15 12:01:55 +0200

    Category - Amélioration de l'affichage des catégories
    Tinymce

---
+++ app/Plugin/Tinymce/Controller/TinymceAppController.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SeoAppController
+ *
+ * @author nicolasmoricet
+ */
+class TinymceAppController extends AppController
+{
+    public function manager_index()
+    {
+        
+    }
+    
+    
+}
+
+?>
