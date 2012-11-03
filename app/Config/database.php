d3198bdb51aaf064aad6206ff8912ea644d4b8e6
JournalTrader <moricet.nicolas@free.fr>
2012-11-03 10:41:42 +0100

    Database

---
+++ app/Config/database.php
@@ -0,0 +1,27 @@
+<?php
+
+class DATABASE_CONFIG {
+    public $default = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
+        'host' => 'localhost',
+        'login' => 'root',
+        'password' => '',
+        'database' => 'metaflex',
+        'prefix' => '',
+        'encoding' => 'UTF8',
+        'port' => '',
+    );
+
+
+    public function __construct()
+    {
+        switch ($_SERVER['SERVER_ADDR'])
+        {
+            case '127.0.0.1':
+                $this->default = $this->default;
+                break;
+        }
+    }
+}
+?>
